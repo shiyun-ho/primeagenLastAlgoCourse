48ee59d9806637b184feed544c4c698d0e90488b
ShiYun <serene.hoshiyun@gmail.com>
2025-07-14 18:19:26 +0800

    Added Queue implementation

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,51 @@
+// TODO: Resolve issue with '='
+type Node<T> {
+  value: T;
+  next?: Node<T>,
+}
+
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
 
-    
+  constructor() {
+    this.head = undefined;
+    this.tail = undefined;
+    length = 0;
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    const node = { value: item } as Node<T>;
+
+    this.length++;
+    if (!this.tail) {
+      this.tail = this.head = node;
+      return;
     }
 
-    enqueue(item: T): void {
+    this.tail.next = node;
+    this.tail = node;
 
-}
-    deque(): T | undefined {
+  }
 
-}
-    peek(): T | undefined {
+  deque(): T | undefined {
+    if (!this.head) {
+      return undefined;
+    }
 
-}
+    this.length--;
+
+    const head = this.head;
+    this.head = this.head.next;
+
+    head.next = undefined;
+
+    return head.value;
+  }
+
+  peek(): T | undefined {
+    return this.head?.value;
+
+  }
 }
